04ea558d60daf29cee3677b4d7e364e87fe62ecf
aubry prieur <aubry.prieur@gmail.com>
2024-03-24 16:47:27 +0100

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { super_admin: 0, seller: 1, buyer: 2 }
+end
