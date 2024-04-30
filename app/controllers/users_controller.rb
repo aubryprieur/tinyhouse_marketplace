3781223b9758ec601db3c5c946942495bbe3d099
aubry prieur <aubry.prieur@gmail.com>
2024-04-30 17:42:53 +0200

    Fixed message display to include both sent and received messages for users, ensuring messages are grouped by house in the user's message view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,14 @@ class UsersController < ApplicationController
     if current_user.super_admin?
       @houses = House.includes(:messages).all
     else
-      @houses = current_user.houses.includes(:messages)
+      # Récupérer les identifiants des maisons pour lesquelles l'utilisateur a envoyé des messages
+      sent_house_ids = current_user.messages.select(:house_id).distinct.pluck(:house_id)
+      # Récupérer les identifiants des maisons pour lesquelles l'utilisateur a reçu des messages
+      received_house_ids = Message.where(house: current_user.houses).select(:house_id).distinct.pluck(:house_id)
+      # Union des deux ensembles d'identifiants
+      all_house_ids = sent_house_ids | received_house_ids  # Utilisation de l'opérateur d'union des tableaux de Ruby
+      # Récupération des maisons correspondant à ces identifiants
+      @houses = House.includes(:messages).where(id: all_house_ids)
     end
   end
 
